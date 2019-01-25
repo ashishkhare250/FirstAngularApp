01310c616197f751005ee8388746a7c8f08cd1df
ashish <ashishkhare250@gmail.com>
2019-01-25 19:31:04 +0530

    first

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor(private route: Router) {
+    route.events.subscribe((s:NavigationEnd) => this.currentURL = s.url) 
+   }
+  
+  currentURL:string;
+
+  ngOnInit() {
+    
+  }
+
+}
